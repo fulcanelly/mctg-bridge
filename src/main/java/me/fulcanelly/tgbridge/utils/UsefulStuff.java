dd783f6b5543a2df44109c3eb4035efa00b52a3d
Herman Semin <scpketer@krampus.pro>
2020-05-03 10:54:50 +0400

    Code cleanup

--- src/main/java/me/fulcanelly/tgbridge/utils/UsefulStuff.java
+++ src/main/java/me/fulcanelly/tgbridge/utils/UsefulStuff.java
@@ -1,48 +1,47 @@
 package me.fulcanelly.tgbridge.utils;
 
-import java.util.Map;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import org.json.simple.JSONObject; 
-import org.json.simple.parser.*; 
-
-import java.net.URL;
-
-import java.io.InputStream;
-import java.io.IOException;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 
 public class UsefulStuff {
     static public JSONObject stringToJSON(String text) {
         try {
-            return (JSONObject)new JSONParser().parse(text);
-        } catch (ParseException e ) {}
+            return (JSONObject) new JSONParser().parse(text);
+        } catch (ParseException ignored) {
+
+        }
 
         return new JSONObject();
     }
 
-    public static String getEnv(String name) {
-        Map<String, String> env = System.getenv();
-        return env.get(name);
-    }
-
     public static String loadPage(String url) {
-        String pageText = new String();
-        try{
+        String pageText = "";
+
+        try {
             InputStream inp = new URL(url)
-                .openConnection()
-                .getInputStream();
+                    .openConnection()
+                    .getInputStream();
 
             byte[] buffer = new byte[4096];
             ByteArrayOutputStream page = new ByteArrayOutputStream();
 
-            int length = 0;
+            int length;
             while ((length = inp.read(buffer)) != -1) {
                 page.write(buffer, 0, length);
             }
             pageText = page.toString();
-        } catch(IOException e) {}
+        } catch (IOException ignored) {
+
+        }
+
         return pageText;
-    } 
+    }
 
 } 
