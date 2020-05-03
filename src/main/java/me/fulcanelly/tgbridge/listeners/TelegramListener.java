7dbfbbbcaece7e16e5c1dc349a7abe1f3a9538e8
Herman Semin <scpketer@krampus.pro>
2020-05-03 10:54:50 +0400

    Code cleanup

--- src/main/java/me/fulcanelly/tgbridge/listeners/TelegramListener.java
+++ src/main/java/me/fulcanelly/tgbridge/listeners/TelegramListener.java
@@ -1,38 +1,23 @@
 package me.fulcanelly.tgbridge.listeners;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import me.fulcanelly.tgbridge.TgBridge;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.ConsoleCommandSender;
-
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import me.fulcanelly.tgbridge.tapi.Message;
 import me.fulcanelly.tgbridge.tapi.events.CommandEvent;
 import me.fulcanelly.tgbridge.tapi.events.MessageEvent;
 import me.fulcanelly.tgbridge.utils.events.pipe.EventHandler;
 import me.fulcanelly.tgbridge.utils.events.pipe.Listener;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 
-class Template {    
-    public static final String dflt_strt = 
-        ChatColor.BLUE + "[tg]" +
-        ChatColor.YELLOW + "[%s]";
-    
-    public static final String dflt_end = 
-        ChatColor.RESET + " %s";
-
-
-    public static final String unkn_strt =
-        ChatColor.BLUE + "[tg]" +
-        ChatColor.YELLOW + "[%s]";
-    
-    public static final String unkn_end =
-        ChatColor.RESET + " sent something";
-
+class Template {
+    public static final String defBeginning = ChatColor.BLUE + "[tg]" + ChatColor.YELLOW + "[%s]";
+    public static final String defEnding = ChatColor.RESET + " %s";
+    public static final String unknownBeginning = ChatColor.BLUE + "[tg]" + ChatColor.YELLOW + "[%s]";
+    public static final String unknownEnding = ChatColor.RESET + " sent something";
 }
 
 public class TelegramListener implements Listener {
@@ -42,66 +27,64 @@ public class TelegramListener implements Listener {
         String text = msg.getText();
         TextComponent result = new TextComponent();
 
-        String begin = null;
-        String ending = null;
-        
+        String beginning;
+        String ending;
+
         String name = msg.getFrom().getName();
 
         //begining
-        if(text == null) {
-            begin = String.format(Template.unkn_strt, name);
-            ending = Template.unkn_end;
+        if (text == null) {
+            beginning = String.format(Template.unknownBeginning, name);
+            ending = Template.unknownEnding;
         } else {
-            begin = String.format(Template.dflt_strt, name);
-            ending = String.format(Template.dflt_end, text);
+            beginning = String.format(Template.defBeginning, name);
+            ending = String.format(Template.defEnding, text);
         }
 
-        result.addExtra(begin);
+        result.addExtra(beginning);
 
         Message reply = msg.getReplyTo();
 
         //reply mark
-        if(!reply.is_null()) {
-            TextComponent replyComponent = formatMessage(reply); 
-            TextComponent сomponent = new TextComponent(ChatColor.GRAY + "(in reply to)");
+        if (!reply.is_null()) {
+            TextComponent replyComponent = formatMessage(reply);
+            TextComponent component = new TextComponent(ChatColor.GRAY + "(in reply to)");
 
-            HoverEvent hEvent = new HoverEvent( 
-                HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(replyComponent).create()
+            HoverEvent hEvent = new HoverEvent(
+                    HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(replyComponent).create()
             );
 
-            сomponent.setHoverEvent(hEvent);
-            result.addExtra(сomponent);
+            component.setHoverEvent(hEvent);
+            result.addExtra(component);
         }
         //ending 
         result.addExtra(ending);
 
         return result;
     }
-    
+
     public TelegramListener(TgBridge bridge) {
         this.bridge = bridge;
     }
-    
+
     void broadcast(TextComponent component) {
         Bukkit
-            .spigot()
-            .broadcast(component);
-        
+                .spigot()
+                .broadcast(component);
+
         ConsoleCommandSender console = bridge.getServer()
-            .getConsoleSender();
+                .getConsoleSender();
 
         console.spigot()
-            .sendMessage(component);
+                .sendMessage(component);
     }
-
-    List<Message> messagesBuffer = new ArrayList<>();
 
     @EventHandler
     public void onMessage(MessageEvent event) {
         String text = event.getText();
-        TextComponent message = null;
+        TextComponent message;
 
-        if(text != null && text.startsWith("/")) {
+        if (text != null && text.startsWith("/")) {
             bridge.tgpipe.emit(new CommandEvent(event.msg));
         } else {
             message = formatMessage(event);
