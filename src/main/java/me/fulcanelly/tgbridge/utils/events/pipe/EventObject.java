704a79023e1f4bbaea1e9da219599112f34df010
Herman Semin <scpketer@krampus.pro>
2020-05-03 10:56:59 +0400

    Code cleanup

--- src/main/java/me/fulcanelly/tgbridge/utils/events/pipe/EventObject.java
+++ src/main/java/me/fulcanelly/tgbridge/utils/events/pipe/EventObject.java
@@ -3,5 +3,5 @@ package me.fulcanelly.tgbridge.utils.events.pipe;
 import me.fulcanelly.tgbridge.utils.events.detector.EventDetectorManager;
 
 public interface EventObject {
-    static EventDetectorManager.Detector detector = null;
+    EventDetectorManager.Detector detector = null;
 }
