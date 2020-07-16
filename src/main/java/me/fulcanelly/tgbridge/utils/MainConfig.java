951f9a4fc5ee7eb5b79786351e2d7a7284351b79
fulcanelly <git config --global user.email>
2020-07-17 02:49:37 +0300

    updated ConfigLoader.java

---
+++ src/main/java/me/fulcanelly/tgbridge/utils/MainConfig.java
@@ -0,0 +1,32 @@
+package me.fulcanelly.tgbridge.utils;
+
+import me.fulcanelly.tgbridge.tools.config.*;
+
+import java.io.File;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@ConfigFille(file = "config.json")
+public class MainConfig {
+    
+    @Saveable
+    public String api_token = "token of telegram bot";
+
+    @Saveable
+    public String chat_id = "id of telegram chat where bot suppose to work";
+
+    @Saveable
+    public Boolean login_manger = false;
+
+    @Saveable
+    public String test_field = "works";
+
+    public ConfigManager<MainConfig> manager;
+
+    public MainConfig(JavaPlugin plugin) {
+        File path = plugin.getDataFolder();
+        manager = new ConfigManager<>(this, path);
+        manager.setOnAbsent(manager::save);
+    }
+    
+}
