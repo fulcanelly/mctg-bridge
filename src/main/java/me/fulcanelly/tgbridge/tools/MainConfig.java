adaddfb5732e9b4964aa2d5bac094cbfc2a1ddb5
fulcanelly <dallasxfulcanelly@gmail.com>
2020-12-26 15:51:41 +0200

    changed chat_id type

--- src/main/java/me/fulcanelly/tgbridge/tools/MainConfig.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/MainConfig.java
@@ -5,19 +5,6 @@ import me.fulcanelly.tgbridge.utils.config.annotations.Optional;
 import me.fulcanelly.tgbridge.utils.config.annotations.Saveable;
 import me.fulcanelly.tgbridge.utils.config.annotations.Nullable;
 
-import java.io.File;
-
-import org.bukkit.plugin.java.JavaPlugin;
-/*
-class LoginManager {
-    Boolean enable;
-};
-class ConfigS {
-    String api_token;
-    String chat_id;
-    LoginManager login_manger;
-}
-*/
 @ConfigFile(file = "config.yml")
 public class MainConfig {
     
@@ -26,7 +13,7 @@ public class MainConfig {
 
     @Saveable
     @Nullable
-    public Integer chat_id;
+    public String chat_id;
 
     @Saveable
     public Boolean login_manger = false;
@@ -36,13 +23,5 @@ public class MainConfig {
     @Nullable
     @Optional
     public String test_field = "works";
-
-   // public ConfigManager<MainConfig> manager;
-
-    /*public MainConfig(JavaPlugin plugin) {
-        File path = plugin.getDataFolder();
-        manager = new ConfigManager<>(this, path);
-        manager.setOnAbsent(manager::save);
-    }*/
     
 }
