2568d2954a5f63ef8dab94440152b66745d5b1f5
fulcanelly <dallasxfulcanelly@gmail.com>
2020-12-26 10:31:28 +0200

    moved

--- src/main/java/me/fulcanelly/tgbridge/utils/config/ConfigFile.java
+++
@@ -1,8 +0,0 @@
-package me.fulcanelly.tgbridge.utils.config;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ConfigFile {
-    String file();
-}
--- src/main/java/me/fulcanelly/tgbridge/utils/config/Saveable.java
+++
@@ -1,6 +0,0 @@
-package me.fulcanelly.tgbridge.utils.config;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Saveable {}
