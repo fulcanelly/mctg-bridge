1639dc32b1baa326db8da5488c151c17caf314f9
fulcanelly <dallasxfulcanelly@gmail.com>
2020-12-26 15:52:01 +0200

    update

--- src/main/java/me/fulcanelly/tgbridge/TgBridge.java
+++ src/main/java/me/fulcanelly/tgbridge/TgBridge.java
@@ -1,29 +1,39 @@
 package me.fulcanelly.tgbridge;
 
 import java.io.File;
-import java.lang.management.ManagementFactory;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.lang.management.ManagementFactory;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.yaml.snakeyaml.Yaml;
 
+import lombok.SneakyThrows;
 import me.fulcanelly.tgbridge.listeners.ActionListener;
 import me.fulcanelly.tgbridge.listeners.TelegramListener;
 import me.fulcanelly.tgbridge.tapi.CommandAction;
 import me.fulcanelly.tgbridge.tapi.CommandManager;
 import me.fulcanelly.tgbridge.tapi.Message;
 import me.fulcanelly.tgbridge.tapi.TGBot;
+import me.fulcanelly.tgbridge.tapi.events.CommandEvent;
 import me.fulcanelly.tgbridge.tapi.events.MessageEvent;
 import me.fulcanelly.tgbridge.tools.stats.StatCollector;
 import me.fulcanelly.tgbridge.tools.stats.StatsTable;
 import me.fulcanelly.tgbridge.tools.DeepLoger;
 import me.fulcanelly.tgbridge.tools.MainConfig;
 import me.fulcanelly.tgbridge.utils.UsefulStuff;
+import me.fulcanelly.tgbridge.utils.config.ConfigManager;
 import me.fulcanelly.tgbridge.utils.events.pipe.EventPipe;
+import net.md_5.bungee.api.ChatColor;
 
 
 class BScheduler extends BukkitRunnable {
@@ -52,8 +62,21 @@ public class TgBridge extends JavaPlugin {
     public CommandManager commands = null;
     public EventPipe tgpipe = new EventPipe();
 
-    public MainConfig config;
-    public String chat;
+    
+    CommandManager getCommandsManager() {
+        return commands;
+    }
+
+    void setCommandManager(CommandManager manager) {
+        this.commands = manager;
+    }
+
+    String chat_id;
+
+    public String getPinnedChatId() {
+       return chat_id;
+    }
+     //String chat;
 
     public TgBridge() {
         instance = this;
@@ -114,6 +137,13 @@ public class TgBridge extends JavaPlugin {
             .getPluginManager()
             .disablePlugin(plugin);
     }
+    int secretTempCode;
+    
+    void generateSecretTempCode() {
+        secretTempCode = new Random().nextInt() % 100000;
+        this.getLogger().info( 
+            ChatColor.GREEN + "secretTempCode is set to " + secretTempCode);
+    }
 
     CommandAction getListCmdHandler() {
         String emptyServerMessage = "Server unfortunately is empty :c";
@@ -128,74 +158,61 @@ public class TgBridge extends JavaPlugin {
         };
     }
 
-    @Override
-    public void onEnable() {
+    private void safeEnable() {
         DeepLoger.initalize(this);
-
-        if (!new File(getDataFolder(), "config.json").exists()) {
-            saveResource("config.json", false);
-        }
-
-        try {
-            config = new MainConfig(this);
-            config.manager.load();
-        } catch(Exception e) {
-            getLogger()
-                .warning(e.getMessage());
-            turnOff();
-            return;
-        }
-
-        chat = config.chat_id;
-
-       // LoginManager loginer = new LoginManager(this);
-
-      /*  if (config.login_manger) {
-            getServer()
-                .getPluginManager()
-                .registerEvents(loginer, this);
-
-            getCommand("log").setExecutor(loginer);
-
-            //System.out.println(loginer.getListener());
-            // tgpipe
-            //   .registerListener(loginer.getListener());
-        }
-*/
         TGBot.setEventPipe(tgpipe);
+
+        if (!new File(getDataFolder(), "config.yml").exists()) {
+            saveResource("config.yml", false);
+        }
+
+        var config = new MainConfig(); 
+
+        new ConfigManager<>(config, this.getDataFolder()).load();
+
+        chat_id = config.chat_id;
 
         bot = new TGBot(config.api_token);
 
         //order of adding detectors is important
         bot.getDetectorManager()
             .addDetector(MessageEvent.detector);
-            
+          //  CommandEvent
         tgpipe
             .registerListener(new TelegramListener(this));
 
-        try {
-            username = bot
-                .getMe()
-                .getUsername();
-        } catch(Exception e) {
-            e.printStackTrace();
+        
+        username = bot
+            .getMe()
+            .getUsername();
 
-            turnOff();
-            return;
-        }
 
         Message.setBot(bot);
-        commands = new CommandManager(username);
+        this.setCommandManager(new CommandManager(username));
+
         StatCollector.initalize(this);
 
+        generateSecretTempCode();
+        
+        if (config.chat_id == null) {
+            this.getLogger().warning(
+                "chat_id is null, use /attach <secretTempCode> to pin one");
+        }
+
         commands
-        /*
-            .addCommand("pin", msg -> {
-                Message replied = msg.getReplyTo();
-                replied.getText();
-                bot.pinChatMessage(
-                    replied.getChat().getId().toString(), replied.getMsgId().toString() );
-            })*/
+            .addCommand("attach", event -> {
+                if (event.getArgs().isEmpty()) {
+                    event.reply("Sepcify secret code");
+
+                } else if (Integer.parseInt(event.args[0]) == secretTempCode) {
+                    config.chat_id = event.getChat().getId().toString();
+                    event.reply("OK, done. Reload plugin");
+                    new ConfigManager<>(config, this.getDataFolder()).save();
+                    this.generateSecretTempCode();
+                } else {
+                    event.reply("Wrong code");
+                }
+            })
             .addCommand("ping", "pong")
             .addCommand("memory", this::getMemory)
             .addCommand("list", this.getListCmdHandler())
@@ -216,6 +233,7 @@ public class TgBridge extends JavaPlugin {
             });
 
         in_listener = new ActionListener(bot, config.chat_id);
+
         getServer()
             .getPluginManager()
             .registerEvents(in_listener, this);
@@ -228,6 +246,32 @@ public class TgBridge extends JavaPlugin {
             .getId()
             .toString();
         message.reply(chat_id);
+    }
+
+    class ReloadException extends Exception {
+
+        private static final long serialVersionUID = 1L;
+    
+    }
+    
+    void startGuard() {
+        try {  
+            safeEnable();
+        } catch (Throwable e) {
+            if (e instanceof ReloadException) {
+                startGuard();
+            }
+
+            e.printStackTrace();
+            turnOff();
+
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public void onEnable() {    
+        new Thread(this::startGuard).start();
     }
 }
 
