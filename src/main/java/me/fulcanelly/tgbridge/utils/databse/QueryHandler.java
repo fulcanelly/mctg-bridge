323de8791b9846425e701e411d91af1b9e65c170
fulcanelly <dallasxfulcanelly@gmail.com>
2020-12-29 00:43:13 +0200

    added functions for parsing databse response into maps

--- src/main/java/me/fulcanelly/tgbridge/utils/databse/QueryHandler.java
+++ src/main/java/me/fulcanelly/tgbridge/utils/databse/QueryHandler.java
@@ -3,6 +3,11 @@ package me.fulcanelly.tgbridge.utils.databse;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -44,6 +49,43 @@ public class QueryHandler {
         
         this.setVars(pstmt, args);
         return pstmt;
+    }
+
+    @SneakyThrows
+    static String getColumnLabelOf(ResultSetMetaData data, int index) {
+        return data.getColumnLabel(index);
+    }
+
+    @SneakyThrows
+    static Object getColumnByIndex(ResultSet set, int index) {
+        return set.getObject(index);
+    }
+
+    @SneakyThrows
+    static public Map<String, Object> parseMapOf(ResultSet set) {
+
+        ResultSetMetaData data = set.getMetaData();
+        
+        return IntStream.range(1, data.getColumnCount() + 1)
+            .boxed()
+            .collect(
+                Collectors.toMap(
+                    i -> getColumnLabelOf(data, i), 
+                    i -> getColumnByIndex(set, i)
+                )
+            );
+    }
+
+    @SneakyThrows
+    static public List< Map<String, Object> > parseListOf(ResultSet set) {
+
+        var list = new ArrayList< Map<String, Object> >();
+
+        while (set.next()) {
+            list.add(parseMapOf(set));
+        }
+
+        return list;
     }
 
     @SneakyThrows
