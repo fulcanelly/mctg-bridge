7a43427aaacc618fa25d02e68ad9f9487b27413f
fulcanelly <dallasxfulcanelly@gmail.com>
2020-12-30 02:30:57 +0200

    tg listener moved tro own package and adjusted to generalization

---
+++ src/main/java/me/fulcanelly/tgbridge/listeners/telegram/TelegramListener.java
@@ -0,0 +1,69 @@
+package me.fulcanelly.tgbridge.listeners.telegram;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+
+import me.fulcanelly.tgbridge.tapi.events.CommandEvent;
+import me.fulcanelly.tgbridge.tapi.events.MessageEvent;
+import me.fulcanelly.tgbridge.utils.events.pipe.EventReactor;
+import me.fulcanelly.tgbridge.utils.events.pipe.Listener;
+import me.fulcanelly.tgbridge.view.MainControll;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class TelegramListener implements Listener {
+
+    final MainControll bridge;
+    final ConsoleCommandSender console;
+
+    public TelegramListener(MainControll bridge) {
+        this.bridge = bridge;
+        console = bridge.getServer()
+            .getConsoleSender();
+    }
+
+    void broadcast(TextComponent component) {
+        Bukkit
+            .spigot()
+            .broadcast(component);
+            
+        console.spigot()
+            .sendMessage(component);
+    }
+
+    boolean isRightChat(MessageEvent event) {
+        return event.getChat()
+            .getId()
+            .toString()
+            .equals(
+                bridge.getPinnedChatId() );
+    }
+
+    @EventReactor
+    public void onMessage(MessageEvent event) {
+        bridge.getActionListener().setActualLast(event.getMsgId());
+
+        if (isCommandEvent(event)) {
+            return;
+        } else if (this.isRightChat(event)) {            
+            broadcast(new EventFormatter(event).getText());
+        }
+    }
+
+    boolean isCommandEvent(MessageEvent event) {
+        String text = event.getText();
+
+        if (text != null && text.startsWith("/")) {
+            bridge.getTelegramPipe().emit(new CommandEvent(event.msg));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @EventReactor
+    public void onCommand(CommandEvent event) {
+        bridge.getCommandManager().tryMatch(event);
+    }
+}
+
