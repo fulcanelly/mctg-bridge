42b39d0c5dc767255100b998cfed068433e05085
fulcanelly <dallasxfulcanelly@gmail.com>
2021-01-14 18:25:28 +0200

    adopted to new database form

--- src/main/java/me/fulcanelly/tgbridge/tools/stats/StatsDatabase.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/stats/StatsDatabase.java
@@ -1,27 +1,30 @@
 package me.fulcanelly.tgbridge.tools.stats;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import lombok.SneakyThrows;
-
-import me.fulcanelly.tgbridge.TelegramBridge;
-import me.fulcanelly.tgbridge.utils.databse.LazySQLActor;
+import me.fulcanelly.tgbridge.utils.databse.SQLiteQueryHandler;
+import me.fulcanelly.tgbridge.utils.reflect.ObjectMapper;
+import me.fulcanelly.tgbridge.utils.async.tasks.AsyncTask;
 
 public class StatsDatabase {
 
-    StatsDatabase(TelegramBridge plugin) {
-        this.sqlhandler = plugin.getSQLhandler();
-    }
+    final SQLiteQueryHandler qhandler;
+    final ObjectMapper mapper;
 
-    LazySQLActor sqlhandler;
+    StatsDatabase(SQLiteQueryHandler sqlite) {//
+        this.mapper = new ObjectMapper();
+        this.qhandler = sqlite;
+    }   
 
     @SneakyThrows
     List<UserStats> getTopByTotalTime(int amount) {
 
-        ResultSet rset = sqlhandler
+        ResultSet rset = qhandler
             .executeQuery("SELECT * FROM user_stats ORDER BY total_time DESC LIMIT ?", amount)
             .waitForResult();
 
@@ -37,7 +40,7 @@ public class StatsDatabase {
     @SneakyThrows
     long getCount() {
 
-        ResultSet result = this.sqlhandler
+        ResultSet result = qhandler
             .executeQuery("SELECT count(*) FROM user_stats")
             .waitForResult();
 
@@ -50,55 +53,49 @@ public class StatsDatabase {
 
     @SneakyThrows
     void initTables() {
-        sqlhandler.execute("CREATE TABLE IF NOT EXISTS user_stats(\n" +
-            "total_time LONG NOT NULL,\n" +
-            "last_point LONG,\n" + 
-            "deaths LONG,\n" +
-            "name STRING PRIMARY KEY NOT NULL\n" +
+        qhandler.execute("CREATE TABLE IF NOT EXISTS user_stats(\n" +
+            " total_time LONG NOT NULL,\n" +
+            " last_point LONG,\n" + 
+            " deaths LONG,\n" +
+            " name STRING PRIMARY KEY NOT NULL\n" +
         ");");
     }
 
     @SneakyThrows
-     UserStats addUserStats(UserStats stats) {
-        sqlhandler.execute(
-            "INSERT INTO user_stats VALUES(?, ?, ?, ?)", 
+    UserStats addUserStats(UserStats stats) {
+        qhandler.execute("INSERT INTO user_stats VALUES(?, ?, ?, ?)", 
             stats.total_time, stats.last_point, stats.deaths, stats.name);
 
         return stats;
     }
 
     @SneakyThrows
-     void updateStats(UserStats stats) {
-        sqlhandler.execute(
-            "UPDATE user_stats SET total_time = ?, last_point = ?, deaths = ? WHERE name = ?",
+    void updateStats(UserStats stats) {
+        qhandler.execute("UPDATE user_stats SET total_time = ?, last_point = ?, deaths = ? WHERE name = ?",
             stats.total_time, stats.last_point, stats.deaths, stats.name);
     }
 
     @SneakyThrows
-     UserStats parserFromResultSet(ResultSet rset) {
-        return new UserStats() {
-            @SneakyThrows
-            UserStats setFields() {
-                total_time = rset.getLong("total_time");
-                last_point = rset.getLong("last_point");
-                deaths = rset.getLong("deaths");
-                name = rset.getString("name");
-                return this;
-            }
-        }.setFields();
+    UserStats parserFromResultSet(ResultSet rset) {
+        return mapper.convertValue(qhandler.parseMapOfResultSet(rset), UserStats.class);
     }
 
     @SneakyThrows
-    public Optional<UserStats> findByName(String name) {   
-        
-        ResultSet result = this.sqlhandler.executeQuery(
-            "SELECT * FROM user_stats WHERE name = ?", name)
-            .waitForResult();
-
-        if (!result.next()) {
+    Optional<UserStats> getOptionalOf(ResultSet set) {
+        if (set.next()) {
+            return Optional.of(parserFromResultSet(set));
+        } else {
             return Optional.empty();
         }
 
-        return Optional.of(parserFromResultSet(result));
     }
+
+    @SneakyThrows
+    public AsyncTask<Optional<UserStats>> findByName(String name) {   
+        return qhandler
+            .executeQuery("SELECT * FROM user_stats WHERE name = ?", name)
+            .andThen(this::getOptionalOf);
+    }
+
+    
 }
