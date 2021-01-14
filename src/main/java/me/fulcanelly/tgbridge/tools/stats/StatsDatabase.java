f8f2b046021b016e5db73e580713e3b31af0e89f
fulcanelly <dallasxfulcanelly@gmail.com>
2021-01-14 18:26:22 +0200

    removed redundant import

--- src/main/java/me/fulcanelly/tgbridge/tools/stats/StatsDatabase.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/stats/StatsDatabase.java
@@ -1,7 +1,6 @@
 package me.fulcanelly.tgbridge.tools.stats;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
