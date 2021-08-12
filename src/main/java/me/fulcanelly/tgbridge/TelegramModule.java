e3d6cc8f41ba4e7ac2dfb86d022570ab9bcd5614
fulcanelly <dallasxfulcanelly@gmail.com>
2021-08-12 23:16:52 +0300

    create foundation for dependecy injection framework

---
+++ src/main/java/me/fulcanelly/tgbridge/TelegramModule.java
@@ -0,0 +1,11 @@
+package me.fulcanelly.tgbridge;
+
+import com.google.inject.AbstractModule;
+
+public class TelegramModule extends AbstractModule { 
+
+    @Override
+    protected void configure() {
+        
+    }
+}
