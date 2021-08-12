4a74fdf6909e94ab3b9e2ee1d142fc64a57d648f
fulcanelly <dallasxfulcanelly@gmail.com>
2021-08-12 22:06:37 +0300

    straightforward solution to send messages in separate thread to not to block events

--- src/main/java/me/fulcanelly/tgbridge/tools/compact/MessageCompactableSender.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/compact/MessageCompactableSender.java
@@ -2,24 +2,48 @@ package me.fulcanelly.tgbridge.tools.compact;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
+
+
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import me.fulcanelly.tgbridge.tapi.TGBot;
 import me.fulcanelly.tgbridge.tools.compact.context.NoteMessageCtx;
 import me.fulcanelly.tgbridge.tools.compact.context.SignedMessageCtx;
 import me.fulcanelly.tgbridge.tools.compact.message.CompactableMessage;
+import me.fulcanelly.tgbridge.tools.compact.visitor.BaseComactableVisitor;
 import me.fulcanelly.tgbridge.tools.compact.visitor.CompactableVisitor;
 import me.fulcanelly.tgbridge.tools.compact.visitor.Compacted;
 import me.fulcanelly.tgbridge.tools.compact.visitor.NoteMessageCompactorVisitor;
 import me.fulcanelly.tgbridge.tools.compact.visitor.PlayerMessageCompactorVisitor;
 
-@RequiredArgsConstructor @Data
+@Data
 public class MessageCompactableSender {
 
     final TGBot bot;
     final Long chatId;
+
+    ArrayBlockingQueue<BaseComactableVisitor> quque = new ArrayBlockingQueue<>(10);
+
+    public MessageCompactableSender(TGBot bot, Long chatId) {
+        this.bot = bot;
+        this.chatId = chatId;
+
+  
+        new Thread(() -> {
+            for (;;) {
+                try {
+                    var comapctor = quque.take();
+                    tryCompactOrSendNew(comapctor);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        
+    }
 
     private final AtomicLong actualLast = new AtomicLong(-1);
     public Optional<CompactableMessage> lastSent = Optional.empty();
@@ -35,7 +59,7 @@ public class MessageCompactableSender {
     } 
     
     <V extends CompactableVisitor & Compacted> 
-    boolean compactUsingCtxAndCompactor(V compactor) {
+    boolean compactUsingCtxAndCompactor(BaseComactableVisitor compactor) {
         return lastSent.map(cpt -> {
                 cpt.accept(compactor);
                 return compactor.isCompacted();
@@ -43,22 +67,23 @@ public class MessageCompactableSender {
             .orElse(false);
     }
  
-    <V extends CompactableVisitor & Compacted & CtxHolder> 
-    boolean tryCompactOrSendNew(V compactor) {
+    boolean tryCompactOrSendNew(BaseComactableVisitor compactor) {
         return Objects.isNull(chatId) ||
             compactUsingCtxAndCompactor(compactor) || 
             setAllLastOfCompactable(compactor.getCtx().send());
     }
 
+    @SneakyThrows
     public void sendAsPlayer(String from, String text) {
-        tryCompactOrSendNew(
+        quque.add(
             new PlayerMessageCompactorVisitor(
                 actualLast, new SignedMessageCtx(bot, chatId, from, text))
         );
     } 
 
+    @SneakyThrows
     public void sendNote(String text) {
-        tryCompactOrSendNew(
+        quque.add(
             new NoteMessageCompactorVisitor(
                 actualLast, new NoteMessageCtx(bot, chatId, text))
         );
