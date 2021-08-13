85cc0b3d37133aa054d6da85bd8d735f5c7e0e14
fulcanelly <dallasxfulcanelly@gmail.com>
2021-08-13 14:47:03 +0300

    add bindings

--- src/main/java/me/fulcanelly/tgbridge/TelegramModule.java
+++ src/main/java/me/fulcanelly/tgbridge/TelegramModule.java
@@ -1,11 +1,54 @@
 package me.fulcanelly.tgbridge;
 
-import com.google.inject.AbstractModule;
+import java.io.File;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+import com.google.inject.util.Providers;
+import java.sql.Connection;
+
+import org.apache.commons.lang.ObjectUtils.Null;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import me.fulcanelly.clsql.databse.SQLQueryHandler;
+import me.fulcanelly.tgbridge.utils.database.SqliteConnectionProvider;
+
+@Data 
 public class TelegramModule extends AbstractModule { 
 
+    JavaPlugin plugin;
+
+    @Provides
+    Connection provideConnection(SqliteConnectionProvider cp) {
+        return cp.getConnection();
+    }
+    
     @Override
     protected void configure() {
+        bind(SqliteConnectionProvider.class)
+            .in(Scopes.SINGLETON);       
         
+        bind(File.class)
+            .annotatedWith(
+                Names.named("plugin_folder")
+            )
+            .toInstance(new File("dasdas"));// plugin.getDataFolder());
+    
     }
+    
+    public static void main(String[] args) {
+        Guice.createInjector(new TelegramModule())
+            .getInstance(Connection.class);
+    }
+
 }
