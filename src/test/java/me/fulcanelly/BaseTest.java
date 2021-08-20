026074df08a1c0e927d361893cf1d74bde1a4e41
fulcanelly <dallasxfulcanelly@gmail.com>
2021-08-20 19:32:55 +0300

    temporary erased base test

--- src/test/java/me/fulcanelly/BaseTest.java
+++ src/test/java/me/fulcanelly/BaseTest.java
@@ -1,16 +1,11 @@
 package me.fulcanelly;
 
-import static org.mockito.Mockito.*;
-
 import org.junit.jupiter.api.Test;
-
-import me.fulcanelly.tgbridge.TelegramBridge;
 
 public class BaseTest {
     
     @Test
     public void test() {
-        TelegramBridge bridge = mock(TelegramBridge.class);
-        System.out.println("work!");
+   
     }
 }
