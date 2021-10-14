41b34e0e280488dfa3313b4b57acaca5b46c0602
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-14 19:02:03 +0300

    fix top command

--- src/main/java/me/fulcanelly/tgbridge/tools/command/TopCommand.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/TopCommand.java
@@ -1,10 +1,13 @@
 package me.fulcanelly.tgbridge.tools.command;
 
+import com.google.inject.Inject;
+
 import me.fulcanelly.tgbridge.tools.command.base.StringReplierBuilder;
 import me.fulcanelly.tgbridge.tools.stats.StatCollector;
 
 public class TopCommand extends StringReplierBuilder {
 
+    @Inject
     public TopCommand(StatCollector scollector) {
         super("top", scollector::getMessage);
     }
