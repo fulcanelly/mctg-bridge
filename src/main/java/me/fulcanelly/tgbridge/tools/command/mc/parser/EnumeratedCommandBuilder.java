45556917fb51bb88e63a325f839230ebafcec640
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-23 14:41:38 +0300

    add class for making enum command arguments

---
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/EnumeratedCommandBuilder.java
@@ -0,0 +1,47 @@
+package me.fulcanelly.tgbridge.tools.command.mc.parser;
+
+import static me.fulcanelly.tgbridge.tools.command.mc.parser.CommandBuilder.named;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class EnumeratedCommandBuilder {
+
+    LinkedList<CommandBuilder> builders = new LinkedList<>();
+    Optional<String> permission = Optional.empty();
+    Consumer<ArgumentsBundle> consumer;
+
+    public static EnumeratedCommandBuilder enumerated(String ...names) {
+        //for ()
+        return new EnumeratedCommandBuilder(names);
+    }
+
+    EnumeratedCommandBuilder(String ...names) {
+        for (var name : names) {
+            builders.add(named(name));
+        }
+    }
+
+    public EnumeratedCommandBuilder setExecutor(Consumer<ArgumentsBundle> consumer) {
+        this.consumer = consumer;
+        return this;
+    }
+    
+    public EnumeratedCommandBuilder setPermission(String perm) {
+        permission = Optional.of(perm);
+        return this;
+    }
+
+
+    public CommandBuilder[] done() {
+        for (var builder : builders) {
+            builder.setExecutor(consumer);
+            if (permission.isPresent()) {
+                builder.setPermission(permission.get());
+            };
+        }
+        return (CommandBuilder[])builders.toArray();
+    }
+
+}
