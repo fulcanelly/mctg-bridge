eaff248ab01a96646164ac146ad628b7fdb71f60
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-23 10:21:14 +0300

    add typed argument builder

--- src/main/java/me/fulcanelly/tgbridge/tools/mastery/ExecutorCommandBuilder.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/mastery/ExecutorCommandBuilder.java
@@ -152,6 +152,42 @@ class Command {
     
 }
 
+class ArgumentBuilder<T> {
+    
+    Argument argument = new Argument();
+
+    static <T>ArgumentBuilder<T> create() {
+        return new ArgumentBuilder<T>();
+    }
+
+    ArgumentBuilder<T> makeOptional() {
+        argument.required = false;
+        return this;
+    }
+
+    ArgumentBuilder<T> setName(String name) {
+        argument.name = name;
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    ArgumentBuilder<T> setDefaultSupplier(Supplier<T> supplier) {
+        argument.defaultSupplier = (Supplier<Object>) supplier;
+        return this;
+    }
+    
+    @SuppressWarnings("unchecked")
+    ArgumentBuilder<T> setParser(Function<String, T> parser) {
+        argument.parser = (Function<String, Object>) parser;
+        return this;
+    }
+    
+    Argument done() {
+        return argument;
+    }
+
+}
+
 class CommandBuilder {
     Command cmd = new Command();
 
