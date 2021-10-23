2a003c00282128f304ec21919eed36a0e9828623
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-23 14:19:28 +0300

    add parse result comment

--- src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/ParseResult.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/ParseResult.java
@@ -1,6 +1,7 @@
 package me.fulcanelly.tgbridge.tools.command.mc.parser;
 
 import java.util.List;
+import java.util.Optional;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,7 @@ import lombok.With;
 public class ParseResult {
     
     List<String> expected, redundant;
-
+    Optional<String> comment;
     
-    static public ParseResult empty = new ParseResult(List.of(), List.of());
+    static public ParseResult empty = new ParseResult(List.of(), List.of(), Optional.empty());
 }
