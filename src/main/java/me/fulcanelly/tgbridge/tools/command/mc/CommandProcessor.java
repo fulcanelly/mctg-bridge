cf6e77b13c9bae93b9af1a3cd8e210f94a212832
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-24 11:31:02 +0300

    wire up things

--- src/main/java/me/fulcanelly/tgbridge/tools/command/mc/CommandProcessor.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/mc/CommandProcessor.java
@@ -1,8 +1,17 @@
 package me.fulcanelly.tgbridge.tools.command.mc;
 
+import static me.fulcanelly.tgbridge.tools.command.mc.parser.CommandBuilder.*;
+
+import me.fulcanelly.tgbridge.tools.command.mc.parser.ArgumentBuilder;
+import me.fulcanelly.tgbridge.tools.command.mc.parser.CommandParser;
+import me.fulcanelly.tgbridge.tools.command.mc.parser.CommandSchema;
+import me.fulcanelly.tgbridge.tools.mastery.ChatSettings;
+
+import me.fulcanelly.tgbridge.tools.twofactor.InGameReceptionUI;
 import me.fulcanelly.tgbridge.utils.events.pipe.Listener;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
@@ -15,45 +24,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
 import me.fulcanelly.clsql.async.tasks.AsyncTask;
 import me.fulcanelly.clsql.databse.SQLQueryHandler;
 
-public class CommandProcessor implements TabCompleter, CommandExecutor {
+public class CommandProcessor implements TabExecutor {
 
 
-    void setup() {
-      
-    }
-
-
-
+    @Inject  
+    CommandSchema commandSchema;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-       
-
+        var parser = new CommandParser(commandSchema, new LinkedList<String>(List.of(args)), sender);
+        parser.evaluate();
         return true;
     }
 
  
-
-
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         return null;
-        /*
-        
-        if (args[1] == "chat") {
-            return List.of("show", "ignore");
-        } else if (args[1] == "login") {
-            return List.of("change", "ignore");
-        } else if (args[1] == "help") {
-        }*/
-
-         
-       // return null;
+    
     }
 
 }
