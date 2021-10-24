234b0ae6df85711c7dae7abc3a73b63ff66f43b3
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-24 16:57:44 +0300

    add template for future argument hinting

--- src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/Argument.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/Argument.java
@@ -1,52 +1,12 @@
 package me.fulcanelly.tgbridge.tools.command.mc.parser;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
 import com.google.common.base.Supplier;
 
 import lombok.ToString;
-
-/**
- * init: cmd args
- * init: cmds subcmd args
- * 
- * cmd: WORD
- * 
- * subcmd: cmd
- *       | cmd | subcmd
- * 
- * arg: WORD:WORD
- *    | WORD:NUMBER
- *  
- * args:
- *     | WORD 
- *     | arg
- *     | arg args
- *  
- * 
- * 
- * ExecutorCommandBuilder builder = new CommandBuilder("tg")
- *      .addSubCmd(
- *          new CommandBuilder("chat")
- *              .addSubCmd(
- *                  new CommandBuilder("show")
- *                      .setReactor(...)
- *              )
- *              .addSubCmd(
-*                   new CommandBuilder("hide")
- *                      .setReactor(...)  
- *              )
- *      )
- *
-* ExecutorCommandBuilder builder = new CommandBuilder("tg")
-*      .addSubCmd(
-*          new CommandBuilder("chat")
-*              .addArg("show")
-*              .addArg("hide")
-*              .setReactor(...)
-*      )
-*/
 
 @ToString
 public class Argument {
@@ -57,6 +17,12 @@ public class Argument {
 
     Optional<String> permission = Optional.empty();
     Function<String, Object> parser = a -> a;
+
+    //Function<Optional<String>, List<String>> expectedSupplier;
+    
+    List<String> getExpected() {
+        return List.of();
+    }
 
     boolean isOptional() {
         return !required;
