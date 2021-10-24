a5b80c5624d1f7f3906c20b3a8793b6f2e59f382
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-24 16:58:38 +0300

    add command and arg suggestions

--- src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/CommandSchema.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/mc/parser/CommandSchema.java
@@ -1,9 +1,11 @@
 package me.fulcanelly.tgbridge.tools.command.mc.parser;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import lombok.Data;
 
@@ -19,13 +21,26 @@ public class CommandSchema {
     Map<String, Argument> argumentByName = new HashMap<>();
 
     Optional<Consumer<ArgumentsBundle>> evaluator = Optional.empty();
-    
+     
+    List<String> getCommandSuggestions(String name) {
+        return this.commandByName.keySet().stream()
+            .filter(cmdName -> cmdName.startsWith(name))
+            .collect(Collectors.toList());
+    }
+
+    List<String> getArgumentSuggestions(String name) {
+        return this.argumentByName.keySet().stream()
+            .filter(argName -> argName.startsWith(name))
+            .map(argName -> argName + ":")
+            .collect(Collectors.toList());
+   }
+
     public String getWrongArgsError() {
         return "wrong arguments error";
     }
 
-    public String getUnknownArgError(String argument) {
-        return "unknown argument: " + argument;
+    public String getUnknownArgError() {
+        return "unknown argument: ";
     }
 
     public String getNotEnoughError() {
