b2f689732638dae9a8545333dc9d9866d276cb92
fulcanelly <dallasxfulcanelly@gmail.com>
2021-10-29 00:51:17 +0300

    fix java.lang.IllegalStateException: Asynchronous player kick

--- src/main/java/me/fulcanelly/tgbridge/tools/command/tg/KickMeCommand.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/command/tg/KickMeCommand.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import me.fulcanelly.tgbridge.tapi.CommandManager;
 import me.fulcanelly.tgbridge.tapi.events.CommandEvent;
@@ -14,9 +15,14 @@ import me.fulcanelly.tgbridge.tools.twofactor.register.SignupLoginReception;
 
 public class KickMeCommand extends OnlinePlayerBoundCommand {
 
+    @Inject 
+    Plugin plugin;
+
     @Override
     public String onBoundOnlinePlayerMessage(CommandEvent event, Player player) {
-        player.kickPlayer("kick by your request");
+        Bukkit.getScheduler().runTask(plugin, it -> {
+            player.kickPlayer("kick by your request");
+        });
         return "You are kicked successfully";
     }
 
