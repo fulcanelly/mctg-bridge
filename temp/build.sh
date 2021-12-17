dc4e3d62183ef2ebe5064c0408b17883ae0d64df
fulcanelly <dallasxfulcanelly@gmail.com>
2021-12-17 23:43:59 +0200

    move in more appropriate place

--- temp/build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf me
-javac  -implicit:class  -cp "src/main/java:tg-bridge-1.3.jar" src/main/java/me/fulcanelly/dither/Main.java -d here
-mv here/me me/
-jar cfev0 dither.jar me.fulcanelly.dither.Main me
