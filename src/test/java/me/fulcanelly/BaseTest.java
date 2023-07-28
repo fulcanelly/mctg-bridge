36ca2f1f3b536a3036dac493fa4a56e62ac11760
fulcanelly <dallasxfulcanelly@gmail.com>
2023-07-28 05:18:04 +0300

    add tests

---
+++ src/test/java/me/fulcanelly/BaseTest.java
@@ -0,0 +1,63 @@
+package me.fulcanelly;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.util.Modules;
+
+import me.fulcanelly.tgbridge.TelegramModule;
+import me.fulcanelly.tgbridge.tools.mastery.ChatSettings;
+import me.fulcanelly.tgbridge.tools.twofactor.InGameReceptionUI;
+
+public class BaseTest {
+
+    Plugin makePluginMock() {
+        var pluginMock = mock(Plugin.class);
+
+        when(pluginMock.getLogger()).thenReturn(mock(Logger.class));
+        when(pluginMock.getDataFolder()).thenReturn(mock(File.class));
+
+        return pluginMock;
+    }
+
+    Module getModule() {
+        return Modules.override(
+                new TelegramModule(makePluginMock())).with(new AbstractModule() {
+                    @Provides
+                    InGameReceptionUI getInGameReceptionUI() {
+                        return mock(InGameReceptionUI.class);
+                    }
+
+                    @Provides
+                    ChatSettings getChatSettings() {
+                        return mock(ChatSettings.class);
+                    }
+                });
+    }
+
+    protected Injector getInjector() {
+        return Guice.createInjector(getModule());
+    }
+
+    @BeforeEach
+    void setupInjector() {
+        System.out.println("Injection");
+
+        var injector = getInjector();
+        injector.injectMembers(this);
+    }
+}
---
+++ src/test/java/me/fulcanelly/CommandProcessorSuggestionsTest.java
@@ -0,0 +1,97 @@
+package me.fulcanelly;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.bukkit.entity.Player;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+
+import me.fulcanelly.tgbridge.tools.command.mc.CommandProcessor;
+import static me.fulcanelly.tgbridge.tools.command.mc.parser.CommandBuilder.*;
+import me.fulcanelly.tgbridge.tools.command.mc.parser.CommandSchema;
+import static me.fulcanelly.tgbridge.tools.command.mc.parser.EnumeratedCommandBuilder.*;
+
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class CommandProcessorSuggestionsTest extends BaseTest {
+
+    @Inject
+    CommandProcessor commandProcessor;
+
+    @Override
+    Module getModule() {
+        return new AbstractModule() {
+            @Provides
+            @Singleton
+            CommandSchema obtainCommandSchema() {
+                return create()
+                        .setName("tg")
+                        .addCommand(
+                                named("chat")
+                                        .addCommand(
+                                                enumerated("show", "hide").done()),
+                                named("account")
+                                        .addCommand(named("register")))
+                        .done();
+            }
+        };
+    }
+
+    private Set<String> obtainUnorderedHintFor(String... arguments) {
+        return commandProcessor.onTabComplete(null, null, null, arguments)
+                .stream()
+                .collect(Collectors.toSet());
+    }
+
+    @Test
+    void testBaseSuggestion() {
+        var suggestion = obtainUnorderedHintFor();
+        assertEquals(
+                "Must suggest 'chat', 'account'",
+                Set.of("chat", "account"),
+                suggestion);
+    }
+
+    @Test
+    void testChildEnumSuggestion() {
+        var suggestion = obtainUnorderedHintFor("chat");
+        assertEquals(
+                "Must suggest 'show', 'hide'",
+                Set.of("show", "hide"),
+                suggestion);
+    }
+
+    @Test
+    void testEmptySuggestionInTheEnd() {
+        var suggestion = obtainUnorderedHintFor("chat", "show");
+        assertEquals(
+                "Must no suggest",
+                Set.of(),
+                suggestion);
+    }
+
+    @Test
+    void testHalfWritten() {
+        var suggestion = obtainUnorderedHintFor("ch");
+        assertEquals(
+                "Must suggest relevant: 'chat'",
+                Set.of("chat"),
+                suggestion);
+    }
+
+    @Test()
+    void testBrokenCommand() {
+
+    }
+}
