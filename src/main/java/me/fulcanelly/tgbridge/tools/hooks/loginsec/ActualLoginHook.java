d92c138c57c391ca2893dec58891e1bef386af6f
fulcanelly <dallasxfulcanelly@gmail.com>
2024-08-26 04:25:48 +0300

    Revert "tmp"
    This reverts commit eb966c27960c573621a5981ac8dab4de3bc5072f.

--- src/main/java/me/fulcanelly/tgbridge/tools/hooks/loginsec/ActualLoginHook.java
+++ src/main/java/me/fulcanelly/tgbridge/tools/hooks/loginsec/ActualLoginHook.java
@@ -1,11 +1,12 @@
 package me.fulcanelly.tgbridge.tools.hooks.loginsec;
 
+
 import com.google.inject.Inject;
-// import com.lenis0012.bukkit.loginsecurity.LoginSecurity;
-// import com.lenis0012.bukkit.loginsecurity.session.AuthService;
-// import com.lenis0012.bukkit.loginsecurity.session.PlayerSession;
-// import com.lenis0012.bukkit.loginsecurity.session.action.ChangePassAction;
-// import com.lenis0012.bukkit.loginsecurity.session.action.RemovePassAction;
+import com.lenis0012.bukkit.loginsecurity.LoginSecurity;
+import com.lenis0012.bukkit.loginsecurity.session.AuthService;
+import com.lenis0012.bukkit.loginsecurity.session.PlayerSession;
+import com.lenis0012.bukkit.loginsecurity.session.action.ChangePassAction;
+import com.lenis0012.bukkit.loginsecurity.session.action.RemovePassAction;
 
 import org.bukkit.plugin.Plugin;
 
@@ -24,30 +25,73 @@ public class ActualLoginHook {
     Plugin basePlugin;
     CommandManager cManager;
 
-    Object getSessionFor(String playerName) {
-        return null;
+    PlayerSession getSessionFor(String playerName) {
+        var player = basePlugin.getServer().getPlayer(playerName);
+        var manager = LoginSecurity.getSessionManager();
+
+        if (player == null) {
+            return manager.getOfflineSession(playerName);
+        }
+
+        return manager.getPlayerSession(player);
     }
 
     String removePassword(CommandEvent event) {
-        return null;
+        var player = reception.getPlayerByTg(event.getFrom().getId());
 
+        if (player.isEmpty()) {
+            return "Your account not bound to minecraft one";
+        }
+        var session = getSessionFor(player.get());
+
+        var response = session.performAction(
+            new RemovePassAction(AuthService.ADMIN, null)
+        );
+        
+        if (response.isSuccess()) {
+            return "Password removed";
+        }
+
+        return response.getErrorMessage();
     }
-
+     
     public void start() {
         registerFew(
-                new ReplierBuilder("removepass", this::removePassword),
-                new ReplierBuilder("changepass", this::changePassword));
+            new ReplierBuilder("removepass", this::removePassword),
+            new ReplierBuilder("changepass", this::changePassword) 
+        );
 
     }
-
+    
     void registerFew(ReplierBuilder... builders) {
         for (var builder : builders) {
             builder.registerCommand(cManager);
         }
     }
+    
 
     String changePassword(CommandEvent event) {
-        return null;
+        var player = reception.getPlayerByTg(event.getFrom().getId());
+
+        if (player.isEmpty()) {
+            return "Your account not bound to minecraft one";
+        }
+
+        if (event.getArgs().isEmpty()) {
+            return "Not enough arguments, type new password";
+        }
+
+        var session = getSessionFor(player.get());
+
+        var response = session.performAction(
+            new ChangePassAction(AuthService.ADMIN, null, event.getArgs().get(0))
+        );
+        
+        if (response.isSuccess()) {
+            return "Password updated";
+        }
+
+        return response.getErrorMessage();
     }
 
 }
